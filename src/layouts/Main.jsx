3f57800549107a15e3f924fd4b3379c8640b74aa
Salman Izhar <Sbinazhar671@gmail.com>
2025-08-05 19:58:28 +0600

    fixed the header error

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/sheard/Header/Header";
 import Footer from "../components/Sheard/Footer/Footer";
+import Header from "../components/sheard/Header/Header";
 
 export default function Main() {
   return (
